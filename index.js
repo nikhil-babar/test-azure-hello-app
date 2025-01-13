57fd6a8f2fb0eeb2a3d064bc463e3a5f84f04c71
Nikhil babar <Nikhil.Babar@msci.com>
2025-01-13 15:43:41 +0530

    main-1

---
+++ index.js
@@ -0,0 +1,13 @@
+const http = require('http')
+const port = 3000;
+
+const server = http.createServer(function (request, response) {
+    response.writeHead(200, {
+        "Content-Type": "text/html"
+    });
+    response.end("<html><body><h1>Hello nikhil babar</h1></body></html>");
+})
+
+server.listen(port);
+
+console.log("Server running on port 3000");
